7fbbb86764543e83cd7b8156e4f47d8d9b3fdd7a
melaniemkwon <melaniemkwon@gmail.com>
2017-07-28 13:34:50 -0700

    Cleaning up the old recyclerview.

--- AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/MainActivity.java
+++ AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/MainActivity.java
@@ -3,33 +3,15 @@ package com.example.android.absolutmixr;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.AsyncTaskLoader;
-import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-import com.example.android.absolutmixr.Model.DrinkItem;
 
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<DrinkItem>>, FragSearch.OnDialogCloseListener {
+public class MainActivity extends AppCompatActivity implements FragSearch.OnDialogCloseListener {
     private static final String TAG = MainActivity.class.getSimpleName();
-
-    private RecyclerView mDrink;
-    private AdapterDrink mAdapter;
-
-    private static final int ADDB_LOADER = 111;
 
     private ViewPager viewPager;    // submenu for Material Design Tab Layout
     private TabLayout tabLayout;    // submenu for Material Design Tab Layout
@@ -39,20 +21,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         super.onCreate(savedInstanceState);
         Log.v(TAG, "Made it here okay");
         setContentView(R.layout.activity_main);
-
-        mDrink = (RecyclerView)findViewById(R.id.recycler);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
-        mDrink.setLayoutManager(layoutManager);
-        mDrink.setHasFixedSize(true);
-        mAdapter= new AdapterDrink();
-        mDrink.setAdapter(mAdapter);
-
-        loadDrinkData();
-
-        /*
-         * Initialize the loader
-         */
-        getSupportLoaderManager().initLoader(ADDB_LOADER, null, this);
 
         Log.v(TAG, "Made it here okay");
 
@@ -103,148 +71,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
     // ##### END MATERIAL DESIGN TAB #####
 
-    public void loadDrinkData(){
-        // ##########################################################
-        // Request that an AsyncTaskLoader performs the GET request
-        URL addbSearchUrl = NetworkUtils.makeURL(); //TODO: MAKE THIS INCLUDE THE QUERY PARAMS
-
-        Bundle queryBundle = new Bundle();
-//        queryBundle.putString(,);
-
-        // Give the LoaderManager an ID and it returns a loader (if one exists)
-        // If it doesn't exist, tell the LoaderManager to create one
-        // If it does exist, tell the LoaderManager to restart it
-        LoaderManager loaderManager = getSupportLoaderManager();
-        Loader<String> addbLoader = loaderManager.getLoader(ADDB_LOADER);
-        if (addbLoader == null) {
-            loaderManager.initLoader(ADDB_LOADER, queryBundle, this);
-        } else {
-            loaderManager.restartLoader(ADDB_LOADER, queryBundle, this);
-        }
-        // ##########################################################
-    }
-
-    // TODO: Don't think this method is all that important.. Think we can remove.
-    private void displayDrinkData(){
-        mDrink.setVisibility(View.VISIBLE);
-        Log.v(TAG, "Made it to displayDrink");
-    }
-
-    // ##### AsyncTaskLoader #####
-    //       Implement methods onCreateLoader, onLoadFinished, and onLoaderReset
-    //       for LoaderManager.LoaderCallbacks<Void>
-    @Override
-    public Loader<ArrayList<DrinkItem>> onCreateLoader(int id, final Bundle args) {
-        Log.d(TAG, "Calling AsyncTaskLoader");
-        return new AsyncTaskLoader<ArrayList<DrinkItem>>(this) {
-
-            ArrayList<DrinkItem> result;
-
-            @Override
-            protected void onStartLoading() {
-                if (args == null) {
-                    return;
-                }
-
-                if (result != null) {
-                    deliverResult(result);
-                } else {
-                    forceLoad();
-                }
-            }
-
-            @Override
-            public ArrayList<DrinkItem> loadInBackground() {
-
-                URL url = NetworkUtils.makeURL();
-
-                try {
-                    String json = NetworkUtils.getResponseFromHttpUrl(url);
-                    result = NetworkUtils.parseJSON(json);
-                    return result;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return null;
-                } catch(JSONException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-
-            @Override
-            public void deliverResult(ArrayList<DrinkItem> data) {
-                super.deliverResult(data);
-                result = data;
-            }
-        };
-    }
-
-    @Override
-    public void onLoadFinished(Loader<ArrayList<DrinkItem>> loader, ArrayList<DrinkItem> result) {
-        if(result != null){
-            Log.d(TAG, "calling setDrinkData");
-
-            displayDrinkData();
-            mAdapter.setDrinkData(result);
-
-            loadDrinkData();
-        } else {
-            Log.d(TAG, "nothing in result");
-        }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<ArrayList<DrinkItem>> result) {}
-
     // ##### When FragSearch fragment closes, do api call and update recyclerview #####
     @Override
     public void closeDialog(String query) {
         // TODO: do api call with query params and update recyclerview with asynctaskloader
         Log.d(TAG, "closeDialog call");
-        loadDrinkData();
+//        loadDrinkData();
     }
 
-    // DONE: replace AsyncTask with AsyncTaskLoader.
-    // TODO: Delete inner class NetworkTask
-//    public class NetworkTask extends AsyncTask<URL, Void, ArrayList<DrinkItem>> {
-//        String query;
-//        NetworkTask (String s ){
-//            query = s;
-//        }
-//
-//        @Override
-//        protected void onPreExecute(){
-//            Log.v(TAG, "Made it here okay");
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected ArrayList<DrinkItem> doInBackground(URL... params) {
-//            ArrayList<DrinkItem> result =null;
-//            URL url = NetworkUtils.makeURL();
-//            try{
-//                String json =NetworkUtils.getResponseFromHttpUrl(url);
-//                result = NetworkUtils.parseJSON(json);
-//
-//                return result;
-//            }catch (IOException e){
-//                e.printStackTrace();
-//                return null;
-//            }catch(JSONException e){
-//                e.printStackTrace();
-//                return null;
-//            }
-//        }
-//
-//        @Override
-//        protected void onPostExecute (ArrayList<DrinkItem> result){
-//            super.onPostExecute(result);
-//            Log.v(TAG, "Made it to onPost");
-//            if(result !=null){
-//                displayDrinkData();
-//                mAdapter.setDrinkData(result);
-//
-//            }
-//        }
-//    }
+    // DONE: replace AsyncTask with AsyncTaskLoader (which is implemented in FragCocktails)
 }

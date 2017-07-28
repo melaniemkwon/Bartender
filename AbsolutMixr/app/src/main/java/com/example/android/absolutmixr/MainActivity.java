91f984fdec6c9a35eb564d9340e9716b82da1dce
Melanie Kwon <melaniemkwon@gmail.com>
2017-07-28 13:42:38 -0700

    UI for MainActivity, AsyncTaskLoader done, Search button. (#3)
    * Update mainactivity to constraint layout. Add search icon to menu.
    
    * Implement Material Design tab layout for cocktails, cabinet, wishlist, and bac calc
    
    * Can't work on moving recyclerview until asynctask update to AsyncTaskLoader.
    
    * Clicking search menu icon brings up a fragment dialogue
    
    * Continue work on FragSearch.java
    
    * Add logging for debugging. Initialize the AsyncTaskLoader.
    
    * Recyclerview now showing results frmo query. AsyncTaskLoader working now.
    
    * Refactor AsynTaskLoader GET request into loadDrinkData method
    
    * Minor clean up
    
    * Migrated all of asynctaskloader to FragCocktails. Now recyclerview is displaying in correct tab.
    
    * Cleaning up the old recyclerview.

--- AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/MainActivity.java
+++ AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/MainActivity.java
@@ -1,26 +1,20 @@
 package com.example.android.absolutmixr;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.example.android.absolutmixr.Model.DrinkItem;
 
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragSearch.OnDialogCloseListener {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private RecyclerView mDrink;
-    private AdapterDrink mAdapter;
+    private ViewPager viewPager;    // submenu for Material Design Tab Layout
+    private TabLayout tabLayout;    // submenu for Material Design Tab Layout
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,72 +22,62 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "Made it here okay");
         setContentView(R.layout.activity_main);
 
-        mDrink = (RecyclerView)findViewById(R.id.recycler);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
-        mDrink.setLayoutManager(layoutManager);
-
-        mDrink.setHasFixedSize(true);
-
-        mAdapter= new AdapterDrink();
-
-        mDrink.setAdapter(mAdapter);
-
         Log.v(TAG, "Made it here okay");
-        loadDrinkData();
+
+        // Implement Material Design Tab Layout
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    private void loadDrinkData(){
-        displayDrinkData();
-        NetworkTask task = new NetworkTask("");
-        task.execute();
-
+    // ##### MENU #####
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    private void displayDrinkData(){
-        mDrink.setVisibility(View.VISIBLE);
-        Log.v(TAG, "Made it to displayDrink");
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemNumber = item.getItemId();
+
+        switch (itemNumber) {
+            case R.id.search:
+                // DONE: launch 'advanced search' fragment
+                openSearch();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
-    public class NetworkTask extends AsyncTask<URL, Void, ArrayList<DrinkItem>> {
-        String query;
-        NetworkTask (String s ){
-            query = s;
-        }
-
-        @Override
-        protected void onPreExecute(){
-            Log.v(TAG, "Made it here okay");
-            super.onPreExecute();
-        }
-
-        @Override
-        protected ArrayList<DrinkItem> doInBackground(URL... params) {
-            ArrayList<DrinkItem> result =null;
-            URL url = NetworkUtils.makeURL();
-            try{
-                String json =NetworkUtils.getResponseFromHttpUrl(url);
-                result = NetworkUtils.parseJSON(json);
-
-                return result;
-            }catch (IOException e){
-                e.printStackTrace();
-                return null;
-            }catch(JSONException e){
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        @Override
-        protected void onPostExecute (ArrayList<DrinkItem> result){
-            super.onPostExecute(result);
-            Log.v(TAG, "Made it to onPost");
-            if(result !=null){
-                displayDrinkData();
-                mAdapter.setDrinkData(result);
-
-            }
-        }
+    // Open FragSearch fragment dialogue
+    private void openSearch() {
+        FragmentManager fm = getSupportFragmentManager();
+        FragSearch fragSearch = new FragSearch();
+        fragSearch.show(fm, "searchfragment");
     }
+    // ##### END MENU #####
+
+    // ##### MATERIAL DESIGN TAB #####
+    public void setupViewPager(ViewPager viewPager){
+        AdapterViewPager pagerAdapter = new AdapterViewPager(getSupportFragmentManager());
+        pagerAdapter.addFragment(new FragCocktails(),"Cocktails");
+        pagerAdapter.addFragment(new FragCabinet(),"Cabinet");
+        pagerAdapter.addFragment(new FragWishlist(), "Wishlist");
+        pagerAdapter.addFragment(new FragBAC(), "BAC Calc");
+        viewPager.setAdapter(pagerAdapter);
+    }
+    // ##### END MATERIAL DESIGN TAB #####
+
+    // ##### When FragSearch fragment closes, do api call and update recyclerview #####
+    @Override
+    public void closeDialog(String query) {
+        // TODO: do api call with query params and update recyclerview with asynctaskloader
+        Log.d(TAG, "closeDialog call");
+//        loadDrinkData();
+    }
+
+    // DONE: replace AsyncTask with AsyncTaskLoader (which is implemented in FragCocktails)
 }
--- AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/NetworkUtils.java
+++ AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/NetworkUtils.java
@@ -97,5 +97,4 @@ public class NetworkUtils {
         }
         return result;
     }
-
 }

b4519505138c3caf0b63a8c07b63c5670d7cea5b
melaniemkwon <melaniemkwon@gmail.com>
2017-08-06 17:35:24 -0700

    Create WishlistDbHelper.

---
+++ AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/Model/WishlistDbHelper.java
@@ -0,0 +1,41 @@
+package com.example.android.absolutmixr.Model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by melaniekwon on 8/6/17.
+ */
+
+public class WishlistDbHelper extends SQLiteOpenHelper {
+
+    static final String DATABASE_NAME = "wishlist.db";
+    static final int DATABASE_VERSION = 1;
+
+    public WishlistDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String SQL_CREATE_WAITLIST_TABLE = "CREATE TABLE " +
+                WishlistContract.WishlistEntry.TABLE_NAME + " (" +
+                WishlistContract.WishlistEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                WishlistContract.WishlistEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                WishlistContract.WishlistEntry.COLUMN_DESCRIPTION + " TEXT," +
+                WishlistContract.WishlistEntry.COLUMN_COLOR + " TEXT," +
+                WishlistContract.WishlistEntry.COLUMN_SKILL + " TEXT," +
+                WishlistContract.WishlistEntry.COLUMN_RATING + " TEXT" +
+                ");";
+
+        db.execSQL(SQL_CREATE_WAITLIST_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + WishlistContract.WishlistEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}

49c8cb413622c612435cea1b5d18889d0182b317
melaniemkwon <melaniemkwon@gmail.com>
2017-08-06 20:16:39 -0700

    Before changing checkbox click.

--- AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/Model/WishlistDbHelper.java
+++ AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/Model/WishlistDbHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class WishlistDbHelper extends SQLiteOpenHelper {
 
     static final String DATABASE_NAME = "wishlist.db";
-    static final int DATABASE_VERSION = 2;
+    static final int DATABASE_VERSION = 3;
 
     public WishlistDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);

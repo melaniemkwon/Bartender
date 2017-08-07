3b7aaed9fbd2a3b97747c0925818078d646e9f1a
melaniemkwon <melaniemkwon@gmail.com>
2017-08-06 17:29:28 -0700

    Add WishlistContract for db

---
+++ AbsolutMixr/app/src/main/java/com/example/android/absolutmixr/Model/WishlistContract.java
@@ -0,0 +1,21 @@
+package com.example.android.absolutmixr.Model;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by melaniekwon on 8/6/17.
+ */
+
+public class WishlistContract {
+    private WishlistContract() {}
+
+    public static final class WishlistEntry implements BaseColumns {
+        public static final String TABLE_NAME = "wishlist";
+
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_COLOR = "color";
+        public static final String COLUMN_SKILL = "skill";
+        public static final String COLUMN_RATING = "rating";
+    }
+}
